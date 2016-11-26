a1cca0f947f6a4a37a47295b7114bf5167f41f54
Darryl Thompson <dthompson98@gmail.com>
2016-11-26 11:18:33 -0600

    Enhancements for payment history and app navigation

---
+++ WebContent/app/js/paymenthistory/paymenthistory_controller.js
@@ -0,0 +1,173 @@
+'use strict';
+
+/**
+ * Controller for PaymentHistory
+ **/
+paymentHistoryModule.controller('PaymentHistoryCtrl', ['PaymentHistory', 'Account', '$scope', '$routeParams', '$http', '$location', '$cookies', 'MessageHandler', 'restURL', function (PaymentHistory, Account, $scope, $routeParams, $http, $location, $cookies, MessageHandler, restURL) {
+        'Account', // edition mode
+                $scope.mode = null;
+        $scope.selectedAccount = "";
+        // list of accounts
+        $scope.accounts = [];
+        // list of paymentHistorys
+        $scope.paymentHistorys = [];
+        $scope.historyEntries = [];
+        // paymentHistory to edit
+        $scope.paymentHistory = null;
+
+        // referencies entities
+        $scope.items = {};
+        // accounts
+        $scope.items.accounts = [];
+
+        $scope.loadAccountList = function() {
+        	
+            $scope.loadAllAccounts();
+            $location.path('/scheduleHistories');
+        };
+        
+        $scope.loadAllAccounts = function() {
+        	
+          Account.getAll().then(
+                    function(success) {
+                        $scope.accounts = success.data;
+                    }, 
+                    MessageHandler.manageError);  
+        };
+        /**
+         * Load all referencies entities
+         */
+        $scope.loadAllReferencies = function () {
+            Account.getAllAsListItems().then(
+                    function (success) {
+                        $scope.items.accounts = success.data;
+                    },
+                    MessageHandler.manageError);
+        };
+
+        /**
+         * Refresh paymentHistorys list
+         */
+        $scope.refreshPaymentHistoryList = function () {
+            try {
+                $scope.paymentHistorys = [];
+                PaymentHistory.getAll().then(
+                        function (success) {
+                            $scope.paymentHistorys = success.data;
+                        },
+                        MessageHandler.manageError);
+            } catch (ex) {
+                MessageHandler.manageException(ex);
+            }
+        };
+        /**
+         * Refresh paymentHistory
+         */
+        $scope.refreshPaymentHistory = function (idpaymentHistory) {
+            try {
+                $scope.paymentHistory = null;
+                PaymentHistory.get(idpaymentHistory).then(
+                        function (success) {
+                            $scope.paymentHistory = success.data;
+                        },
+                        MessageHandler.manageError);
+            } catch (ex) {
+                MessageHandler.manageException(ex);
+            }
+        };
+        /**
+         * Refresh account payment history
+         */
+        $scope.loadHistoryRecords =  function(accountId) {
+        	$scope.selectedAccount = accountId;
+            $scope.refreshAccountHistory(accountId);
+        };
+        
+        $scope.refreshAccountHistory = function (id) {
+            try {
+                $scope.historyEntries = null;
+                PaymentHistory.getAccountHistory(id).then(
+                        function (success) {
+                            $scope.historyEntries = success.data;
+                        },
+                        MessageHandler.manageError);
+            } catch (ex) {
+                MessageHandler.manageException(ex);
+            }
+        };
+        
+        
+        
+        /**
+         * Go to the paymentHistorys list page
+         */
+        $scope.goToPaymentHistoryList = function () {
+            $scope.refreshPaymentHistoryList();
+            $location.path('/paymentHistory');
+        };
+        /**
+         * Go to the paymentHistory edit page
+         */
+        $scope.goToPaymentHistory = function (idpaymentHistory) {
+            $scope.refreshPaymentHistory(idpaymentHistory);
+            $location.path('/paymentHistory/' + idpaymentHistory);
+        }
+
+        // Actions
+
+        /**
+         * Save paymentHistory
+         */
+        $scope.save = function () {
+            try {
+                MessageHandler.cleanMessage();
+                var save;
+                if ($scope.mode === 'create') {
+                    save = PaymentHistory.create;
+                } else {
+                    save = PaymentHistory.update;
+                }
+                save($scope.paymentHistory).then(
+                        function (success) {
+                            MessageHandler.addSuccess('save ok');
+                            $scope.paymentHistory = success.data;
+                        },
+                        MessageHandler.manageError);
+            } catch (ex) {
+                MessageHandler.manageException(ex);
+            }
+        };
+        /**
+         * Delete paymentHistory
+         */
+        $scope.delete = function (idpaymentHistory) {
+            try {
+                MessageHandler.cleanMessage();
+                PaymentHistory.delete(idpaymentHistory).then(
+                        function (success) {
+                            $scope.goToPaymentHistoryList();
+                        },
+                        MessageHandler.manageError);
+            } catch (ex) {
+                MessageHandler.manageException(ex);
+            }
+        };
+
+        // Main
+        MessageHandler.cleanMessage();
+        if ($location.path().endsWith('/new')) {
+            // Creation page
+            $scope.paymentHistory = {};
+            $scope.mode = 'create';
+            $scope.loadAllReferencies();
+            $scope.bookorderitem = null;
+        } else if ($routeParams.idpaymentHistory != null) {
+            // Edit page
+            $scope.loadAllReferencies();
+            $scope.refreshPaymentHistory($routeParams.idpaymentHistory);
+        } else {
+            // List page
+            $scope.refreshPaymentHistoryList();
+        }
+
+    }]);
---
+++ WebContent/app/js/paymenthistory/paymenthistory_module.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Module for PaymentHistory */
+
+var paymentHistoryModule = angular.module('paymentHistory.module', ['myApp']);
+
+/**
+ * Module for paymentHistory
+ */
+paymentHistoryModule.config(['$routeProvider', function($routeProvider) {
+    // Pages routes
+    $routeProvider.when('/scheduleHistories',    {templateUrl: 'partials/paymenthistory/paymenthistory_profile.html', controller: 'PaymentHistoryCtrl'});
+    $routeProvider.when('/paymentHistory',    {templateUrl: 'partials/paymenthistory/paymenthistory_list.html', controller: 'PaymentHistoryCtrl'});
+    $routeProvider.when('/paymentHistory/new', {templateUrl: 'partials/paymenthistory/paymenthistory_form.html', controller: 'PaymentHistoryCtrl'});
+    $routeProvider.when('/paymentHistory/:idpaymentHistory', {templateUrl: 'partials/paymenthistory/paymenthistory_form.html', controller: 'PaymentHistoryCtrl'});
+}]);
